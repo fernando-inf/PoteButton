46eb82072ab78eaf67008241ecd51f2b887d3352
fernando-inf <85811973+fernando-inf@users.noreply.github.com>
2021-09-04 13:31:30 -0300

    Update PoteButton.ino

--- PoteButton/PoteButton.ino
+++ PoteButton/PoteButton.ino
@@ -1,7 +1,8 @@
 //Fuente Original: https://www.programmingelectronics.com/make-one-button-functionality-two-arduino/
 //Adaptado por fernando-inf@hotmail.com
-//Potenciometro en Pulsador Digital= Al apretar y soltar rápido el Valor de la Variable (Velocity) se incrementa.
-                                   //Al apretar 500ms (o mas tiempo) y soltar, el Valor de la Variable decrece.
+//Potenciometro en Pulsador Digital= 
+//Al apretar y soltar rápido el Valor de la Variable (Velocity) se incrementa.
+//Al apretar 500ms (o mas tiempo) y soltar, el Valor de la Variable decrece.
 
 
 //#include <MIDI.h>
@@ -9,42 +10,61 @@
 
 float tiempo_transcurrido = 0;//Inicia en 0
 int intervalo1 = 5;
-int intervalo2 = 500;        
+int intervalo2 = 500;
 int Velocity = 0; //El valor inicial deberia obtenerse mediante MIDI Input (señal recibida del Software) al seleccionar un preset, en caso de aplicarlo se esa forma borrar "= 0".
 const int VelocityMax = 3;
 //const int Ch = 1; //Canal MIDI= 1
 
-void PoteButton() 
-              {
-                  while (digitalRead (2)==LOW){ delay(50); tiempo_transcurrido = tiempo_transcurrido + 50;}
-                
-                  if (tiempo_transcurrido >= intervalo2)
-                            {
-                              Velocity--; if (Velocity < 0) Velocity = VelocityMax;
-                              //MIDI.sendControlChange(51, Velocity, Ch); //(CC, Valor, Canal)
-                              for (int i = 0; i <= 3; i++) {if (Velocity == i) {digitalWrite(i+3, HIGH);}  else {digitalWrite(i+3, LOW);}}
-      
-                            } 
-                  else if(tiempo_transcurrido >= intervalo1)
-                            {
-                              Velocity++; if (Velocity > VelocityMax) Velocity = 0;
-                              //MIDI.sendControlChange(51, Velocity, Ch);//(CC, Valor, Canal)
-                              for (int i = 0; i <= 3; i++) {if (Velocity == i) {digitalWrite(i+3, HIGH);}  else {digitalWrite(i+3, LOW);}}
-                            }      
-                  tiempo_transcurrido = 0; //Reinicia el tiempo transcurrido a 0
-              }
+void PoteButton()
+{
+  while (digitalRead (2) == LOW) {
+    delay(50);
+    tiempo_transcurrido = tiempo_transcurrido + 50;
+  }
 
-void setup() 
-              {
-                //MIDI.begin(MIDI_CHANNEL_OMNI);//MIDI_CHANNEL_OMNI= Lee los mensajes de todos los Canales, si esta vacio solo lee el Canal 1 en MIDI IN.
-                //Serial.begin(115200); // Si conecta directo a MIDI - sin puerto serial: 31250
-                pinMode(2, INPUT_PULLUP);//PIN DIGITAL D2 para Pulsador
-                for (int i = 3; i <= 6; i++){pinMode(i,OUTPUT);}//PINES DIGITALES para LEDs: D3 a D6
-                
-              }
+  if (tiempo_transcurrido >= intervalo2)
+  {
+    Velocity--; if (Velocity < 0) Velocity = VelocityMax;
+    //MIDI.sendControlChange(51, Velocity, Ch); //(CC, Valor, Canal)
+    for (int i = 0; i <= 3; i++) {
+      if (Velocity == i) {
+        digitalWrite(i + 3, HIGH);
+      }  else {
+        digitalWrite(i + 3, LOW);
+      }
+    }
 
-void loop() 
-              {
-                if(Velocity==0){digitalWrite(3, HIGH);}//Inicio
-                PoteButton();
-              }
+  }
+  else if (tiempo_transcurrido >= intervalo1)
+  {
+    Velocity++; if (Velocity > VelocityMax) Velocity = 0;
+    //MIDI.sendControlChange(51, Velocity, Ch);//(CC, Valor, Canal)
+    for (int i = 0; i <= 3; i++) {
+      if (Velocity == i) {
+        digitalWrite(i + 3, HIGH);
+      }  else {
+        digitalWrite(i + 3, LOW);
+      }
+    }
+  }
+  tiempo_transcurrido = 0; //Reinicia el tiempo transcurrido a 0
+}
+
+void setup()
+{
+  //MIDI.begin(MIDI_CHANNEL_OMNI);//MIDI_CHANNEL_OMNI= Lee los mensajes de todos los Canales, si esta vacio solo lee el Canal 1 en MIDI IN.
+  //Serial.begin(115200); // Si conecta directo a MIDI - sin puerto serial: 31250
+  pinMode(2, INPUT_PULLUP);//PIN DIGITAL D2 para Pulsador
+  for (int i = 3; i <= 6; i++) {
+    pinMode(i, OUTPUT); //PINES DIGITALES para LEDs: D3 a D6
+  }
+
+}
+
+void loop()
+{
+  if (Velocity == 0) {
+    digitalWrite(3, HIGH); //Inicio
+  }
+  PoteButton();
+}
